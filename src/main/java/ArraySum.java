be2167a0fb19b620655113a0f2305b050422113e
Danilos-Prog <daniilmutaliev2001@gmail.com>
2022-04-04 15:57:14 +0400

    feature: change the structure of ArraySum class

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -1,15 +1,22 @@
 public class ArraySum {
     Integer n;
-    Integer m;
 
-    public ArraySum(Integer n, Integer m) {
-        if (n == null || m == null) throw new NullPointerException("n or m can't be equal null!");
-        if (m > n) throw new IllegalArgumentException("m should be less than n or equal n!");
-        this.n = n;
-        this.m = m;
+    public ArraySum(Integer n) {
+        setN(n);
     }
 
-    public Integer getSum(){
+    public void setN(Integer n) {
+        if (n == null) throw new NullPointerException("n or m can't be equal null!");
+        if (n <= 0) throw new IllegalArgumentException("n should be more than 0");
+        this.n = n;
+    }
+
+    public Integer getN() {
+        return n;
+    }
+
+    public Integer getSum(Integer m){
+        if (m > n) throw new IllegalArgumentException("m should be less than n or equal n!");
         return null;
     }
 }
